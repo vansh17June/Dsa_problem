e05714982bf1767834c97fc1650045c21409ec5a
vansh17June <devlopervansh@gmail.com>
2026-06-16 10:07:42 +0530

    Time: 9 ms (20.08%), Space: 39.8 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a'&&s[i]<='z'){
+                result+=s[i];
+            }else if(s[i]=='*'&&result.size()>0){
+                result.pop_back();
+            }else if(s[i]=='#'&&result.size()>0){
+                result+=result;
+            }else if(result.size()>0){
+                reverse(result.begin(),result.end());
+            }
+
+        }
+        return result;
+    }
+};
